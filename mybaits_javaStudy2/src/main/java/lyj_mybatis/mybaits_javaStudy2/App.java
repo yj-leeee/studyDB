5111ef7b037a4568c849656e2e9b72749aaf8212
yj-leeee <asas87189@gmail.com>
2024-11-29 11:07:06 +0900

    마이바티스 프로젝트 2생성 2장 63p부터 이어서

---
+++ mybaits_javaStudy2/src/main/java/lyj_mybatis/mybaits_javaStudy2/App.java
@@ -0,0 +1,13 @@
+package lyj_mybatis.mybaits_javaStudy2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
