327a87fc341a6b87776d69d7ca65360be45b34f8
yj-leeee <asas87189@gmail.com>
2024-11-29 10:59:27 +0900

    패키지에 클래스 추가, 패키지 경로 ldg > lyj로 수정

---
+++ mybatis_jdbcStudy2/test/lyj/mybatis/repository/session/CommentJdbcRepositoryTest.java
@@ -0,0 +1,78 @@
+package lyj.mybatis.repository.session;
+
+import java.util.*;
+
+import lyj.mybatis.model.Comment;
+
+public class CommentJdbcRepositoryTest {
+	private final CommentJdbcRepository commentJdbcRepository = new CommentJdbcRepository();
+
+	public void testSelectCommentByPrimaryKey() {
+		Long commentNo = 1L;
+		Comment comment = commentJdbcRepository.selectCommentByPrimaryKey(commentNo);
+
+		System.out.println(comment);
+	}
+//	private void testSelectAllComments() {
+//		private void testSelectAllComments() 메서드 구현
+//		- 클래스의 멤버변수인 commentJdbcRepository로 테스트
+//		- selectAllComments()메서드를 호출하여 결과를 출력
+//		1장 72p
+//힌트 :testSelectCommentByPrimaryKey()메서드와 유사하게 동작
+//	main()메서드에서 해당 테스트 메서드를 호출하게 만들것
+//	}
+
+	public void testInsertComment() {
+		Long commentNo = 1L;
+		String userId = "fromm0";
+		Date regDate = Calendar.getInstance().getTime();
+		String commentContent = "test";
+
+		Comment comment = new Comment();
+		comment.setCommentNo(commentNo);
+		comment.setUserId(userId);
+		comment.setCommentContent(commentContent);
+		comment.setRegDate(regDate);
+		commentJdbcRepository.deleteComment(commentNo);
+		Integer result = commentJdbcRepository.insertComment(comment);
+
+		System.out.println(result);
+	}
+
+	public void testUpdateComment() {
+		Long commentNo = 1L;
+		String commentContent = "test";
+
+		Comment comment = new Comment();
+		comment.setCommentNo(commentNo);
+		comment.setCommentContent(commentContent);
+		Integer result = commentJdbcRepository.updateComment(comment);
+
+		System.out.println(result);
+	}
+
+	public void testDeleteComment() {
+		Long commentNo = 1L;
+		Integer result = commentJdbcRepository.deleteComment(commentNo);
+
+		System.out.println(result);
+	}
+
+	public void testSelectCommentByCondition() {
+		Map<String, Object> condition = new HashMap<String, Object>();
+		condition.put("commentNo", 2L);
+		//		condition.put("commentNoForeach", Arrays.asList(1L, 2L, 3L));
+		condition.put("commentNoForeach", new ArrayList<Long>());
+		commentJdbcRepository.selectCommentByCondition(condition);
+	}
+
+	public static void main(String[] args) {
+		CommentJdbcRepositoryTest test = new CommentJdbcRepositoryTest();
+
+		test.testSelectCommentByPrimaryKey();
+		test.testInsertComment();
+		test.testUpdateComment();
+		test.testDeleteComment();
+		test.testSelectCommentByCondition();
+	}
+}
