e957401d29f88d42e2f50da0cd86ad9b55386ed9
yj-leeee <asas87189@gmail.com>
2024-12-02 23:20:47 +0900

    reply클래스 변수 선언후 자동으로 getter/setter생성 Soruce-Generate Getters and Setters

---
+++ mybatis_javaStudy/src/ldg/mybatis/model/Reply.java
@@ -0,0 +1,42 @@
+package ldg.mybatis.model;
+
+import java.util.Date;
+
+public class Reply {
+	private Long replyNo;
+	private Long commentNo;
+	private String userId;
+	private String replyContent;
+	private Date regDate;
+	public Long getReplyNo() {
+		return replyNo;
+	}
+	public void setReplyNo(Long replyNo) {
+		this.replyNo = replyNo;
+	}
+	public Long getCommentNo() {
+		return commentNo;
+	}
+	public void setCommentNo(Long commentNo) {
+		this.commentNo = commentNo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getReplyContent() {
+		return replyContent;
+	}
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+}
