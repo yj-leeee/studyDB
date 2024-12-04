75c87431f2858f10f09f6b2b4a43e5fb949ae2ab
yj-leeee <asas87189@gmail.com>
2024-12-04 13:11:32 +0900

    상속을 통해 sqlSessionFactory를 받아서 사용(계속 재사용singletone)

--- mybatis_webStudy/src/ldg/mybatis/repository/AbstractRepository.java
+++ mybatis_webStudy/src/ldg/mybatis/repository/AbstractRepository.java
@@ -9,22 +9,22 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public abstract class AbstractRepository {
-	private static SqlSessionFactory sqlSessionFactory;
-
-	static {
-		setSqlSessionFactory();
-	}
-
-	private static void setSqlSessionFactory() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-		} catch (IOException e) {
-			throw new IllegalArgumentException(e);
-		}
-		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-	}
+//	private static SqlSessionFactory sqlSessionFactory;
+//
+//	static {
+//		setSqlSessionFactory();
+//	}
+//
+//	private static void setSqlSessionFactory() {
+//		String resource = "mybatis-config.xml";
+//		InputStream inputStream;
+//		try {
+//			inputStream = Resources.getResourceAsStream(resource);
+//		} catch (IOException e) {
+//			throw new IllegalArgumentException(e);
+//		}
+//		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//	}
 
 	protected SqlSessionFactory getSqlSessionFactory() {
 		return sqlSessionFactory;
