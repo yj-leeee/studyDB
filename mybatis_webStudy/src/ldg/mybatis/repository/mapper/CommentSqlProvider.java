b4ddc5f4f530c8e1c2551ef47f8dd5d014ef1d3b
yj-leeee <asas87189@gmail.com>
2024-12-05 13:38:38 +0900

    @Provider 애노테이션을 사용해 동적 SQL을 만드는 다른 클래스와 메서드를 지정 가능 @SelectProvider (8장에 자세한 설명)

--- mybatis_webStudy/src/ldg/mybatis/repository/mapper/CommentSqlProvider.java
+++ mybatis_webStudy/src/ldg/mybatis/repository/mapper/CommentSqlProvider.java
@@ -9,12 +9,12 @@ import static org.apache.ibatis.jdbc.SqlBuilder.WHERE;
 import java.util.Map;
 
 public class CommentSqlProvider {
-	public String selectCommentByCondition(Map<String, Object> condition) {
-		BEGIN();
-		SELECT("comment_no, user_id, comment_content, reg_date");
-		FROM("comment");
-		WHERE("comment_no = #{commentNo}");
-
-		return SQL();
+//	public String selectCommentByCondition(Map<String, Object> condition) {
+//		BEGIN();
+//		SELECT("comment_no, user_id, comment_content, reg_date");
+//		FROM("comment");
+//		WHERE("comment_no = #{commentNo}");
+//
+//		return SQL();
 	}
 }
